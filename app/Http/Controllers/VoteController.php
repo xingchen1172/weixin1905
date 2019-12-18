9dd09485e2fa38eda2479ac7b3193e8c4e48017c
wzc <wzc@qq.com>
2019-12-18 11:20:41 +0000

    传

--- app/Http/Controllers/VoteController.php
+++ app/Http/Controllers/VoteController.php
@@ -22,24 +22,25 @@ class VoteController extends Controller
         //处理业务逻辑
         
         $openid = $user_info['openid'];
-        $key = 's:vote:zhangsan';
+        $key = 'ss:vote:zhangsan';
 
         //判断是否已经投过票
-        if(Redis::sIsMember($key,$user_info['openid'])){
+        if(Redis::zrank($key,$user_info['openid'])){
             echo "已经投过票了";
         }else{
-            Redis::Sadd($key,$openid);
+            Redis::Zadd($key,time(),$openid);
         }
-        Redis::Sadd($key,$openid);
+        // Redis::Sadd($key,$openid);
 
-        $menbers = Redis::Smembers($key);       //获取所有投票人的openid
-        $total = Redis::Scard($key);        //统计投票总人数
-        echo "投票总人数 :" .$total;
-        echo '<hr>';
+        $total = Redis::zCard($key);        //统计投票总人数
+        echo '投票总人数 :' .$total;echo'</br>';
+        $menbers = Redis::zRange($key,0,-1,true);       //获取所有投票人的openid
+        // echo "投票总人数 :" .$total;
+        // echo '<hr>';
         echo '<pre>';print_r($menbers);echo '</pre>';
-
-
-
+        foreach($menbers as $k=>$v){
+            echo "用户 :" .$k. '投票时间 :' .date('Y-m-d H:i:s',$v);echo '</br>';
+        }
         // $redis_key = 'vote';
         // $number = Redis::incr($redis_key);
         // echo "投票成功,当前票数 :" .$number;
