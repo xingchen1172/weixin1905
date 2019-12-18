404692bf25e5c6ca5758eb889bb02213a77458a1
wzc <wzc@qq.com>
2019-12-18 10:26:35 +0000

    bug

--- app/Http/Controllers/VoteController.php
+++ app/Http/Controllers/VoteController.php
@@ -20,9 +20,22 @@ class VoteController extends Controller
         $user_info = $this->getUserInfo($data['access_token'],$data['openid']);
 
         //处理业务逻辑
-        $redis_key = 'vote';
-        $number = Redis::incr($redis_key);
-        echo "投票成功,当前票数 :" .$number;
+        
+        $openid = $user_info['openid'];
+        $key = 's:vote:zhangsan';
+        Redis::Sadd($key,$openid);
+
+        $menbers = Redis::Smembers($key);       //获取所有投票人的openid
+        $total = Redis::Scard($key);        //统计投票总人数
+        echo "投票总人数 :" .$total;
+        echo '<hr>';
+        echo '<pre>';print_r($menbers);echo '</pre>';
+
+
+
+        // $redis_key = 'vote';
+        // $number = Redis::incr($redis_key);
+        // echo "投票成功,当前票数 :" .$number;
     }
     /**
      * 根据code获取access_token
