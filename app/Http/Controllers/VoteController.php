5ad16dd2b46d42d59a9d05ed307a9d17d6d9aae0
wzc <wzc@qq.com>
2019-12-18 08:52:40 +0000

    提交投票

--- app/Http/Controllers/VoteController.php
+++ app/Http/Controllers/VoteController.php
@@ -18,6 +18,10 @@ class VoteController extends Controller
         //获取用户信息
         $user_info = $this->getUserInfo($data['access_token'],$data['openid']);
 
+        //处理业务逻辑
+        $redis_key = 'vote';
+        $number = Redis::incr($redis_key);
+        echo "投票成功,当前票数 :" .$number;
     }
     /**
      * 根据code获取access_token
@@ -25,15 +29,18 @@ class VoteController extends Controller
     protected function getAccessToken($code){
         $url = 'https://api.weixin.qq.com/sns/oauth2/access_token?appid='.env('WX_APPID').'&secret='.env('WX_APPSECRET').'&code='.$code.'&grant_type=authorization_code';
         $json_data = file_get_contents($url);
-        return json_decode($json_data,true);
-        
+        return json_decode($json_data,true);       
     }
     /**获取用户基本信息 */
     protected function getUserInfo($access_token,$openid)
     {
         $url = 'https://api.weixin.qq.com/sns/userinfo?access_token='.$access_token.'&openid='.$openid.'&lang=zh_CN';
         $json_data = file_get_contents($url);
-        $user_info = json_decode($json_data,true);
-        echo '<pre>';print_r($user_info);echo '</pre>';
+        $data =  json_decode($json_data,true);
+        if(isset($data['errcode'])){
+            //TODO  错误处理
+            die('出错了 40001 ');           //40001  标识获取用户信息失败
+        }
+        return $data;     //返回用户信息
     }
 }
