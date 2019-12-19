b901d368051aace20955b0a167c78a53776ddd02
wzc <wzc@qq.com>
2019-12-19 10:05:33 +0000

    保存用户信息

--- app/Http/Controllers/VoteController.php
+++ app/Http/Controllers/VoteController.php
@@ -18,6 +18,10 @@ class VoteController extends Controller
 
         //获取用户信息
         $user_info = $this->getUserInfo($data['access_token'],$data['openid']);
+        //保存用户信息
+        $userinfo_key = 'h:u:'.$data['openid'];
+        Redis::hMset($userinfo_key,$user_info);
+
 
         //处理业务逻辑
         
@@ -35,11 +39,15 @@ class VoteController extends Controller
         $total = Redis::zCard($key);        //统计投票总人数
         echo '投票总人数 :' .$total;echo'</br>';
         $menbers = Redis::zRange($key,0,-1,true);       //获取所有投票人的openid
-        // echo "投票总人数 :" .$total;
-        // echo '<hr>';
+        echo "投票总人数 :" .$total;
+        echo '<hr>';
         echo '<pre>';print_r($menbers);echo '</pre>';
         foreach($menbers as $k=>$v){
             echo "用户 :" .$k. '投票时间 :' .date('Y-m-d H:i:s',$v);echo '</br>';
+            $u_k = 'h:u:'.$k;
+            $u = Redis::hMget($u_k,['openid','nickname','sex']);
+            echo '<pre>';print_r($u);echo '</pre>';
+            
         }
         // $redis_key = 'vote';
         // $number = Redis::incr($redis_key);
@@ -64,5 +72,24 @@ class VoteController extends Controller
             die('出错了 40001 ');           //40001  标识获取用户信息失败
         }
         return $data;     //返回用户信息
+    }
+
+    public function hashTest()
+    {
+        $uid = 1000;
+        $key = 'h:user_info:uid:'.$uid;
+
+        $user_info = [
+            'uid'        => $uid,
+            'user_name'  => 'zhangsan',
+            'email'      =>  '3228682711@qq.com',
+            'age'        => 22,
+            'sex'         =>1
+        ]; 
+        Redis::hWset($key,$user_info);
+        die;
+        echo '<hr>';
+        $u = Redis::hGetAll($key);
+        echo '<pre>';print_r($u);echo '</pre>';
     }
 }
