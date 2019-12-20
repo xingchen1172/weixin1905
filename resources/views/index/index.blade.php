460023e8ca5d5cb2a3890063673901cbd1a6d51b
wzc <wzc@qq.com>
2019-12-20 03:02:10 +0000

    o

--- resources/views/index/index.blade.php
+++ resources/views/index/index.blade.php
@@ -464,8 +464,8 @@
 			<div class="row">
 				<div class="col s6">
 					<div class="content">
-						<img src="img/product-new1.png" alt="">
-						<h6><a href="">Fashion Men's</a></h6>
+						<a href="{{url('goods/index')}}"><img src="img/product-new1.png" alt=""></a>
+						<h6><a href="{{url('goods/index')}}">Fashion Men's</a></h6>
 						<div class="price">
 							$20 <span>$28</span>
 						</div>
