42d4226546047dbdca5e196500d53f264cbeb8c2
wzc <wzc@qq.com>
2019-12-20 00:20:12 +0000

    完善

--- app/Http/Controllers/Index/IndexController.php
+++ app/Http/Controllers/Index/IndexController.php
@@ -27,7 +27,7 @@ class IndexController extends Controller
         $data = [
             'u' => $user_info
         ];
-       return view('index/index');
+       return view('index/index',$data);
     }
 
     /**
