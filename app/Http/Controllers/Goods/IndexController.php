cbb76324032049c2df2e2b6ab4181b3926e174f4
wzc <wzc@qq.com>
2019-12-23 00:48:36 +0000

    完善作业

--- app/Http/Controllers/Goods/IndexController.php
+++ app/Http/Controllers/Goods/IndexController.php
@@ -4,10 +4,17 @@ namespace App\Http\Controllers\Goods;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Model\Goods;
 
 class IndexController extends Controller
 {
-    public function index(){
-        return view('goods/index');
+    public function index(Request $request){
+        $goods_id = $request->input('id');
+        $goods = Goods::find($goods_id);
+        // echo '<pre>';print_r($goods);echo '</pre>';
+        $data = [
+            'goods'     => $goods
+        ];
+        return view('goods/index',$data);
     }
 } 
